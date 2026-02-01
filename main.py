d2285e5cd34d6da2d22e989bddfd6d7d8b14b859
33shuhei-dev <imoimo33shuhei@gmail.com>
2026-02-01 18:00:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-python
-print("daily board bot runnining")
+pyton
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "daily board bot running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
